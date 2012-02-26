0ef1c3482804fa0b0ffb42f751175bdd93c672ed
Ian <typppo@gmail.com>
2012-02-25 18:41:41 -0800

    more messing around

--- amazon.js
+++ amazon.js
@@ -2,6 +2,7 @@ var  _ = require('underscore')
   , OperationHelper = require('apac').OperationHelper
   , freebase = require('freebase')
   , winston = require('winston')
+  , record = require('./record.js')
   , config = require('./config.js')
 
 var opHelper = new OperationHelper({
@@ -13,6 +14,11 @@ var opHelper = new OperationHelper({
 var EXCLUDE_BINDINGS = ['Amazon Instant Video', 'Kindle Edition',
     'MP3 Download', 'Personal Computers', ];
 
+var MAP_BINDINGS = {
+  'Blu-ray': 'Video',
+  'DVD': 'Video',
+}
+
 
 function search(keyword, opts, cb) {
   // Search Amazon for a keyword
@@ -23,35 +29,60 @@ function search(keyword, opts, cb) {
   //sorting http://docs.amazonwebservices.com/AWSECommerceService/latest/DG/SortingbyPopularityPriceorCondition.html
   //search http://docs.amazonwebservices.com/AWSECommerceService/latest/DG/CommonItemSearchParameters.html
 
-  freebase.search(keyword, console.log);
+  freebase.search(keyword, function(freebase_results) {
+    console.log(freebase_results);
 
-  console.log('Searching...', keyword);
-  opHelper.execute('ItemSearch', {
-    'SearchIndex': 'All',
-    'Keywords': keyword,
-    'ResponseGroup': 'ItemAttributes,Offers',
-    'Availability': 'Available',
-    //'MinimumPrice': 333.50,
-    //'Sort': 'salesrank',
-  }, function(error, results) {
-    if (error) {
-      console.log('Error: ' + error + "\n")
-      return;
+    if (freebase_results.length > 0) {
+      if (freebase_results.length > 1
+        && freebase_results[0].search.score
+          - freebase_results[1].search.score < 2.0) {
+        // don't coerce; too close
+        // remember this so we can improve later
+        record.ambiguous_query(keyword);
+      }
+      else {
+        console.log('Coercing', keyword, 'to', freebase_results[0].name);
+        // put in list for classifer training later
+        record.coercion(keyword, freebase_results[0].name);
+        keyword = freebase_results[0].name;
+      }
     }
-    if (results.Items.Request.IsValid == 'False') {
-      console.log(results.Items.Request.Errors);
-    }
-    else {
-      var bindings = {};
-      _.map(results.Items.Item, function(item) {
-        var binding = item.ItemAttributes.Binding;
-        if (!bindings[binding])
-          bindings[binding] = 0;
-        bindings[binding]++;
-      });
-      console.log(bindings);
-      console.log(results.Items.Item.length);
-    }
+
+    console.log('Searching...', keyword);
+    opHelper.execute('ItemSearch', {
+      'SearchIndex': 'All',
+      'Keywords': keyword,
+      'ResponseGroup': 'ItemAttributes,Offers',
+      'Availability': 'Available',
+      //'MinimumPrice': 333.50,
+      //'Sort': 'salesrank',
+    }, function(error, results) {
+      if (error) {
+        console.log('Error: ' + error + "\n")
+        return;
+      }
+      if (results.Items.Request.IsValid == 'False') {
+        console.log(results.Items.Request.Errors);
+      }
+      else {
+        var bindings = {};
+        _.map(results.Items.Item, function(item) {
+          var binding = item.ItemAttributes.Binding;
+          binding = MAP_BINDINGS[binding] || binding;
+          if (!binding || EXCLUDE_BINDINGS.indexOf(binding) > -1) {
+            // ignore
+          }
+          else {
+            if (!bindings[binding])
+              bindings[binding] = 0;
+            bindings[binding]++;
+          }
+        });
+        console.log(bindings);
+        console.log(results.Items.Item.length);
+
+      }
+    });
   });
 }
 
---
+++ record.js
@@ -0,0 +1,16 @@
+
+function coercion(from, to) {
+
+}
+
+function ambiguous_query(q) {
+
+}
+
+module.exports = {
+
+  coercion: coercion,
+  ambiguous_query: ambiguous_query,
+
+
+}
