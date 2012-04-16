9efce6c9f6c23ab03260399b82b167e3993ae934
Ian <typppo@gmail.com>
2012-04-15 18:28:25 -0700

    add hierarchy stuff

---
+++ hierarchy.js
@@ -0,0 +1,28 @@
+// Amazon browse node hierarchy
+
+var arboreal = require('./lib/arboreal.js')
+
+var bn_index = {};
+
+(function loadHierarchyData() {
+
+
+})()
+
+function distanceFromNodeName(bn, name) {
+
+}
+
+function distanceFromBrowseNode(bn, bn_find) {
+
+}
+
+function browseNodeExists(name) {
+
+}
+
+module.exports = {
+  distanceFromNodeName: distanceFromNodeName,
+  browseNodeExists: browseNodeExists,
+
+}
