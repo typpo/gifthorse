78eb01ded65bf2c25dd3ccc7063f0b782054cae3
typpo <ian@typpo.us>
2012-04-16 14:36:04 -0400

    browse node finding distance is beginning to work

--- hierarchy.js
+++ hierarchy.js
@@ -5,7 +5,8 @@ var Arboreal = require('./lib/arboreal.js')
   , _ = require('underscore')
   , assert = require('assert')
 
-var bn_index = {};
+var bn_index = {};    // map from unique id to single node
+var name_index = {};  // map from name to list of nodes
 var tree;
 
 (function loadHierarchyData() {
@@ -17,7 +18,7 @@ var tree;
   function process_line(line) {
     // Returns true if parent has already been added and the node was added,
     // or if line is superfluous
-    var parts = line.split('|');
+    var parts = line.split('\t');
     if (parts.length < 4)
       return true;  // skip this line
     var name = parts[0];
@@ -32,32 +33,46 @@ var tree;
 
     var node;
     if (parent_id === 'null') {
-      node = tree.appendChild(name);
+      node = tree.appendChild({
+        name: name,
+        id: id,
+      });
       node = node.children[node.children.length-1];
       bn_index[id] = node;
     }
     else {
       var parent_node = bn_index[parent_id];
       if (parent_node) {
-        node = parent_node.appendChild(name);
-        node = node.children[node.children.length-1];
+        node = parent_node.appendChild({
+          name: name,
+          id: id,
+        });
       }
-      else
+      else {
         return false;
+      }
+
       bn_index[id] = node;
+      if (!name_index[name])
+        name_index[name] = [];
+      name_index[name].push(node);
     }
     return true;
   }
 
-  _.map(lines, process_line);
+  _.map(lines, function(line) {
+    if (!process_line(line))
+      deferred_children.push(line);
+  });
 
   while (deferred_children.length > 0) {
-    var child_line = deferred_children.pop();
+    var child_line = deferred_children.shift();
 
     if (!process_line(child_line)) {
       deferred_children.push(child_line);
     }
   }
+  console.log('Done loading browse node hierarchy.');
 
   function iterator(node) {
     var depth = "", i;
@@ -65,23 +80,51 @@ var tree;
     console.log([depth, node.data].join(" "));
   }
 
-  tree.traverseDown(iterator);
+  //tree.traverseDown(iterator);
 })()
 
-function distanceFromNodeName(bn, name) {
-
+function closestDistanceFromNodeName(bn_id, name) {
+  var nodes = name_index[name];
+  return _.min(_.map(nodes, function(node) {
+    return distanceBetweenBrowseNodes(node.data.id, bn);
+  }));
 }
 
-function distanceFromBrowseNode(bn, bn_find) {
+function distanceBetweenBrowseNodes(id_a, id_b) {
+  // TODO right now this assumes one is in the subtree of the other
+  var a = bn_index[id_a];
+  var b = bn_index[id_b];
 
+  if (!a)
+    throw new Error('No such browse node: ' + id_a);
+  if (!b)
+    throw new Error('No such browse node: ' + id_b);
+
+  function getDist(start_node, id_find) {
+    var depth = Number.MAX_VALUE;
+    var found_node = false;
+    start_node.traverseDown(function(node) {
+      if (node.data.id == id_find) {
+        found_node = true;
+        depth = node.depth - start_node.depth;
+        return false;
+      }
+    });
+    return depth;
+  }
+
+  return Math.min(getDist(a, id_b), getDist(b, id_a));
 }
 
 function browseNodeExists(name) {
-
+  return name_index[name] && name_index[name].length > 0;
 }
 
 module.exports = {
-  distanceFromNodeName: distanceFromNodeName,
+  closestDistanceFromNodeName: closestDistanceFromNodeName,
   browseNodeExists: browseNodeExists,
-
+  distanceBetweenBrowseNodes: distanceBetweenBrowseNodes,
 }
+console.log(distanceBetweenBrowseNodes('2210604011','2206260011'))
+console.log(distanceBetweenBrowseNodes('374783011','3409906011'))
+console.log(closestDistanceFromNodeName('374783011','Elephant'))
