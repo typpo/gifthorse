46148e192860b0d4399f14ac347a3505ab9de27e
typpo <ian@typpo.us>
2012-04-17 18:31:15 -0400

    change pearson calc to not use closures

--- suggest.js
+++ suggest.js
@@ -49,21 +49,21 @@ function pearson(v1, v2) {
   // }
   //
 
-  //vs = [(v1[val],v2[val]) for val in v1 if val in v2]
   var vs = [];
   var n = 0;
-  _.map(v1, function(val) {
+  for (var i=0; i < v1.length; i++) {
+    var val = v1[i];
     if (val in v2) {
       vs.push([v1[val], v2[val]]);
       n++;
     }
-  });
+  }
   if (n < 1)
     return 0;
 
   var sum1=0,sum2=0,sum1_sq=0,sum2_sq=0,p_sum=0;
-
-  _.map(vs, function(vv) {
+  for (var i=0; i < vs.length; i++) {
+    var vv = vs[i];
     var v1 = vv[0],
       v2 == vv[1];
 
@@ -72,7 +72,7 @@ function pearson(v1, v2) {
     sum1_sq+=v1*v1
     sum2_sq+=v2*v2
     p_sum+=v1*v2
-  });
+  }
 
   // Calculate Pearson score
   var num = p_sum-(sum1*sum2/n)
