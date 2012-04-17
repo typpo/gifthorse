fb3e8cf74f172f2b9282e67122896540e8390bd1
typpo <ian@typpo.us>
2012-04-17 19:26:20 -0400

    add mongo logging

--- app/brain/log_behavior.js
+++ app/brain/log_behavior.js
@@ -8,12 +8,20 @@ var mongo = require('mongodb')
 var COLL_NAME = 'queries';
 
 // Takes a list of queries
+// cb(err, queryId)
 function recordQueries(queries, cb) {
   mutil.getCollection(COLL_NAME, function(err, collection) {
     if (err) {
       cb(true);
       return;
     }
+
+    collection.insert({queries: queries}, function(err, docs) {
+      if (!err && docs.length > 0)
+        cb(null, docs[0]._id);
+      else
+        cb(true);
+    });
 
   });
 }
@@ -25,6 +33,11 @@ function recordResults(qid, results, cb) {
       return;
     }
 
+    collection.update({_id: new mongo.ObjectID(qid)}, {results:results},
+      function(err) {
+        cb(err);
+    });
+
   });
 
 }
@@ -35,6 +48,23 @@ function recordClick(qid, rid, cb) {
       cb(true);
       return;
     }
+
+    collection.findOne({_id: new mongo.ObjectID(qid)},
+      function(err, obj) {
+        if (err || obj.results.length < rid + 1) {
+          cb(true);
+          return;
+        }
+
+        obj.results[rid].clicked = true;
+        // TODO record click in some other places too, probably
+
+        collection.update({_id: new mongo.ObjectID(qid)}, {results:obj.results},
+          function(err) {
+            cb(err);
+        });
+    });
+
 
   });
 }
